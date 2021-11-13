b4a618cf2063577ee286dce246a556e998711ab7
ivymmurithi <ivymurithi@gmail.com>
2021-11-13 22:16:58 +0300

    make the array to contain strings and add preventDefault

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-let submit = document.querySelector('.button');
+const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-let maleNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
-let femaleNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama];
-let days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
 
-function calculate () {
-  let theDate = document.getElementById('date').value;
+function getName(event) {
+   event.preventDefault();
   let theGender = document.getElementById('gender').value;
-
-  let findDay = new Date(theDate).getDay();
+  let theDate = document.getElementById('date').value;
   
-}
+  let findDay = new Date(theDate).getDay();
+
+  if (theGender === "Male"){
+    return (`${days[findDay]} is the day you were born and ${maleNames[findDay]} is your Akan name`)
+  } else 
+  if (theGender === "Female"){
+    return (`${days[findDay]} is the day you were born and ${[femaleNames]} is your Akan name`)
+  } else {
+    return ('You have not selected your gender!')
+  }
+};
+
+// function displayOutput() {
+//   let theOutput = document.getElementById('output');
+//   theOutput.innerHTML = analyse();
+
+// }
