fcd650abfd60a686663f079284d1db73f66f9b9c
ivymmurithi <ivymurithi@gmail.com>
2021-11-13 17:29:45 +0300

    add a variable that will return the day of the week from their birthday input

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let theDate = document.getElementById('date').value;
-let theGender = document.getElementById('gender').value;
 let submit = document.querySelector('.button');
 
 let maleNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
 let femaleNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama];
 let days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
+
+function calculate () {
+  let theDate = document.getElementById('date').value;
+  let theGender = document.getElementById('gender').value;
+
+  let findDay = new Date(theDate).getDay();
+  
+}
