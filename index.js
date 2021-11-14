c9eb4a4364c0d38baed947ded0b2bcdc5ae25f8d
ivymmurithi <ivymurithi@gmail.com>
2021-11-14 13:38:01 +0300

    add display output function to return results in textarea

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ const femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
-function getName(event) {
-   event.preventDefault();
+function getName() {
   let theGender = document.getElementById('gender').value;
   let theDate = document.getElementById('date').value;
   
   let findDay = new Date(theDate).getDay();
+  
 
   if (theGender === "Male"){
-    return (`${days[findDay]} is the day you were born and ${maleNames[findDay]} is your Akan name`)
+    displayOutput (`${days[findDay]} is the day you were born and ${maleNames[findDay]} is your Akan name`)
   } else 
   if (theGender === "Female"){
-    return (`${days[findDay]} is the day you were born and ${[femaleNames]} is your Akan name`)
+    displayOutput (`${days[findDay]} is the day you were born and ${femaleNames[findDay]} is your Akan name`)
   } else {
-    return ('You have not selected your gender!')
+    displayOutput ('You have not selected your gender!')
   }
 };
 
-// function displayOutput() {
-//   let theOutput = document.getElementById('output');
-//   theOutput.innerHTML = analyse();
-
-// }
+function displayOutput(sentence) {
+  let theOutput = document.getElementById('output');
+  theOutput.innerHTML = sentence;
+}
