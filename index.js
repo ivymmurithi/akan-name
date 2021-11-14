4f7d75ff4a3386b2ecace42de05d7e76e1233706
ivymmurithi <ivymurithi@gmail.com>
2021-11-14 15:01:33 +0300

    add validation for the date input

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ function getName() {
   
   let findDay = new Date(theDate).getDay();
   
-
+  if (findDay < 0 || findDay > 6 || findDay === null || isNaN(findDay)) {
+    displayOutput ("Choose a date!");
+    return;
+  }
+  
   if (theGender === "Male"){
     displayOutput (`${days[findDay]} is the day you were born and ${maleNames[findDay]} is your Akan name`)
   } else 
   if (theGender === "Female"){
     displayOutput (`${days[findDay]} is the day you were born and ${femaleNames[findDay]} is your Akan name`)
   } else {
-    displayOutput ('You have not selected your gender!')
+    displayOutput ('Select your gender!')
   }
 };
 
